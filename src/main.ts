fdcb33128394523b5d5bcdb51604a0036f202e4b
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-15 16:00:11 +0100

    added skills

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { Header } from "./components/Header";
+import { Skills } from "./components/Skills";
 import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 ${Header()}
 <p>This is the first JS-DOM site.</p>
+${Skills()}
 `;
